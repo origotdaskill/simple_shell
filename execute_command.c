4aab79d28677b432af36db1194aaac4e95a6ef2c
origotdaskill <mr.walid.ori@gmaill.com>
2023-12-14 23:40:35 +0100

    ori commit

--- execute_command.c
+++ execute_command.c
@@ -15,26 +15,56 @@ int execute_command(char *command)
 {
     pid_t pid;
     int status;
+    int pipe_fd[2];
+    char buffer[1024];
+    ssize_t bytesRead;
+
+     if (pipe(pipe_fd) == -1) {
+        perror("simple_shell");
+        return 1;
+    }
+
 
     pid = fork();
 
     if (pid == 0)
     {
+        close(pipe_fd[0]); 
+
+        if (dup2(pipe_fd[1], STDOUT_FILENO) == -1) {
+            perror("simple_shell");
+            close(pipe_fd[1]);
+            exit(EXIT_FAILURE);
+        }
+
+        close(pipe_fd[1]);
+
         if (execlp(command, command, NULL) == -1)
         {
             perror("simple_shell");
+            exit(EXIT_FAILURE);
         }
-        exit(EXIT_FAILURE);
     }
     else if (pid < 0)
     {
         perror("simple_shell");
+        close(pipe_fd[0]);
+        close(pipe_fd[1]);
+        return 1;
     }
     else
     {
+        close(pipe_fd[1]);
+
+        while ((bytesRead = read(pipe_fd[0], buffer, sizeof(buffer))) > 0) {
+            write(STDOUT_FILENO, buffer, bytesRead);
+        }
+
+        close(pipe_fd[0]);
+
         do
         {
-            pid = waitpid(pid, &status, WUNTRACED);
+            waitpid(pid, &status, WUNTRACED);
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
 
