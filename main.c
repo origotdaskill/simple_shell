a7997175cb4484584025c3edd3850c7e6a54a92f
whorri <whorri@numericway.net>
2023-12-15 10:20:01 +0100

    ori got new commit

--- main.c
+++ main.c
@@ -1,36 +1,79 @@
-#include "shell.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
+#define BUFFER_SIZE 1024
 
 /**
- * main - Entry point of the simple shell
- * Return: 0 on success
+ * main - Simple UNIX command line interpreter
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-    char input[MAX_INPUT_SIZE];
+    char buffer[BUFFER_SIZE];
+    ssize_t read_bytes;
+    size_t len = 0;
 
     while (1)
     {
-        display_prompt();
+        printf("$ ");
 
-        if (fgets(input, sizeof(input), stdin) == NULL)
+        read_bytes = getline(&buffer, &len, stdin);
+
+        if (read_bytes == -1)
         {
-            printf("\n");
-            break;
+            if (feof(stdin))
+            {
+                printf("\n");
+                break;
+            }
+            else
+            {
+                perror("Error reading input");
+                exit(EXIT_FAILURE);
+            }
         }
 
-        input[strcspn(input, "\n")] = '\0';
+        buffer[strcspn(buffer, "\n")] = '\0';
 
-        if (strlen(input) > 0)
+        pid_t pid = fork();
+
+        if (pid == -1)
         {
-            if (execute_command(input) == 0)
+            perror("Fork failed");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid == 0)
+        {
+            char *args[] = {buffer, NULL};
+            if (execve(buffer, args, NULL) == -1)
             {
-                break;
+                perror("Command not found");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            int status;
+            if (waitpid(pid, &status, 0) == -1)
+            {
+                perror("Error waiting for child process");
+                exit(EXIT_FAILURE);
+            }
+            if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+            {
+                fprintf(stderr, "Command failed with exit status %d\n", WEXITSTATUS(status));
             }
         }
     }
 
+    free(buffer);
+
     return 0;
 }
+
